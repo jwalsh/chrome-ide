f657469a4d734ccf8faa32fda321e46f043699fb
Jason Walsh <jason.walsh@memorylane.com>
2011-08-09 22:46:37 -0700

    Heroku / socketio test

--- web.js
+++ web.js
@@ -13,11 +13,11 @@ server
 
 io.listen(server);
 
-io.sockets.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
-});
+// io.sockets.on('connection', function (socket) {
+//   socket.emit('news', { hello: 'world' });
+//   socket.on('my other event', function (data) {
+//     console.log(data);
+//   });
+// });
 
 console.log("Running http://localhost:" + port + "/");
